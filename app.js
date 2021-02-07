b73c4a20a05ebf32d914d6a0e178042e93ca2fdc
Mihir0411 <ahmihir11@gmail.com>
2021-02-07 22:01:09 +0600

    assignment5

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const searchBtn = document.getElementById('search_button');
 searchBtn.addEventListener('click', () => {
     const inputFoodName = document.getElementById('foodNameInput').value;
 
-    if(inputFoodName === ""){
-        console.log("empty");
-    }
     getFoodName(inputFoodName);
 });
 
-const apiBase = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
+const api = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
 const getFoodName = foodName => {
-    const url = `${apiBase}${foodName}`;
+    const url = `${api}${foodName}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayFoods(data));
@@ -21,7 +18,6 @@ const getFoodName = foodName => {
 const displayFoods = foods =>{
     const item = foods.meals;
     const parentDiv = document.getElementById('food-container');
-    console.log(item);
     item.forEach(meal => {
         const foodDiv = document.createElement('div');
         foodDiv.className = 'food'
@@ -37,25 +33,25 @@ const displayFoods = foods =>{
 
 
 const apiDetail = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
+
 const getFoodDetails = foodDetails => {
-    const url = `${apiDetail}${foodDetails}`;
+    const url = `${apiDetail}${foodDetails}`
     fetch(url)
-    .then(Response => Response.json())
-    .then(data => foodsDetails(data.meals))
+    .then (res => res.json())
+    .then(data => foodsDetails(data))
 }
 
 const foodsDetails = food =>{
-    console.log(food)
-    const countryDetail = document.getElementById('foodsDetails')
-    countryDetail.innerHTML=`
+    const foodDetail = document.getElementById('foodsDetails')
+    foodDetail.innerHTML=`
     <h5 class="asd">${food.strArea}</h5>
-    <p>Population${food.strIngredient1}</p>
-    <p>Population${food.strIngredient2}</p>
-    <p>Population${food.strIngredient3}</p>
-    <p>Population${food.strIngredient4}</p>
-    <p>Population${food.strIngredient5}</p>
-    <p>Population${food.strIngredient6}</p>
-    <p>Population${food.strIngredient7}</p>
-    <p>Population${food.strIngredient8}</p>
+    <p>${food.strIngredient1}</p>
+    <p>${food.strIngredient2}</p>
+    <p>${food.strIngredient3}</p>
+    <p>${food.strIngredient4}</p>
+    <p>${food.strIngredient5}</p>
+    <p>${food.strIngredient6}</p>
+    <p>${food.strIngredient7}</p>
+    <p>${food.strIngredient8}</p>
     `
 }
